77db1f7ba2468a9d3f68811ff824ed16072a0549
s-A301-16 <s-A301-16@CAMPUS.UEK.KRAKOW.PL>
2024-10-28 19:57:53 +0100

    28-10

--- main.js
+++ main.js
@@ -1,7 +1,37 @@
+
+
+
+
+function showForm() {
+    document.getElementById('customer-list').style.display = "none";
+    document.getElementById('customer-form').style.display = "block";
+}
+
+function showList() {
+    document.getElementById('customer-list').style.display = "block";
+    document.getElementById('customer-form').style.display = "none";
+
+    let ul = document.getElementById('customer-list-data');
+    ul.innerHTML = 'Lista klientow';
+    for (let customer of customersList) {
+        ul.innerHTML += `<li>${customer.nazwaFirmy} <button class="btn btn-primary" onclick=showCustomer(`${customer.id}`)</li>`
+    }
+
+}
+
+var customersList = [];
+
+function showCustomer(customerId) {
+    showForm();
+    let customer = customersList.filter()
+    addDataToForm();
+}
+
 function saveData(event) {
     event.preventDefault();
 
-    const clientData = {
+    let clientData = {
+        id: Math.floor(Math.random() * 10001),
         nazwaFirmy: document.getElementById('nazwaFirmy').value,
         nip: document.getElementById('nip').value,
         miasto: document.getElementById('miasto').value,
@@ -13,24 +43,54 @@ function saveData(event) {
         branza: document.getElementById('branza').value,
         aktywny: document.getElementById('aktywny').checked
     };
+
     console.log(clientData);
+    customersList.push(clientData);
+    showList()
+    console.log(customersList);
+
     return false;
 };
 
-function wstawDane() {
-    document.getElementById('nazwaFirmy').value = "Przykładowa Firma";
-    document.getElementById('nip').value = "123-456-32-18";
-    document.getElementById('miasto').value = "Warszawa";
-    document.getElementById('ulica').value = "Przykładowa 1";
-    document.getElementById('numerDomu').value = "1";
-    document.getElementById('numerMieszkania').value = "2";
-    document.getElementById('kodPocztowy').value = "00-001";
-    document.getElementById('uwagi').value = "Brak uwag";
-    document.getElementById('branza').value = "1";
-    document.getElementById('aktywny').checked = true;
+function getCustomer() {
+    return {
+        nazwaFirmy: "Przykładowa Firma",
+        nip: "123-456-32-18",
+        miasto: "Warszawa",
+        ulica: "Przykładowa 1",
+        numerDomu: "1",
+        numerMieszkania: "2",
+        kodPocztowy: "00-001",
+        uwagi: "Brak uwag",
+        branza: "1",
+        aktywny: true
+    }
+}
 
-    const inputs = document.querySelectorAll('input, textarea, select');
-    inputs.forEach(input => {
-        input.disabled = true;
-    });
+function getEmptyCustomer() {
+    return {
+        nazwaFirmy: "",
+        nip: "",
+        miasto: "",
+        ulica: "",
+        numerDomu: "",
+        numerMieszkania: "",
+        kodPocztowy: "",
+        uwagi: "",
+        branza: "",
+        aktywny: ""
+    }
+}
+
+function addDataToForm(newCustomer) {
+    document.getElementById('nazwaFirmy').value = newCustomer.nazwaFirmy;
+    document.getElementById('nip').value = newCustomer.nip;
+    document.getElementById('miasto').value = newCustomer.miasto;
+    document.getElementById('ulica').value = newCustomer.ulica;
+    document.getElementById('numerDomu').value = newCustomer.numerDomu;
+    document.getElementById('numerMieszkania').value = newCustomer.numerMieszkania;
+    document.getElementById('kodPocztowy').value = newCustomer.kodPocztowy;
+    document.getElementById('uwagi').value = newCustomer.uwagi;
+    document.getElementById('branza').value = newCustomer.branza;
+    document.getElementById('aktywny').checked = newCustomer.aktywny;
 }
