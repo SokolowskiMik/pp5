9826dc521a7db85b6d395e8b2c02a97208c153e2
s-A301-04 <s-A301-04@CAMPUS.UEK.KRAKOW.PL>
2024-12-02 19:36:46 +0100

    update

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InvoiceModule } from './invoice/invoice.module';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, InvoiceModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Mikołaj S';
 }
+
+
---
+++ my-app/src/app/invoice/components/customer-form/customer-form.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Customer } from '../../models/customer';
+import { Invoice } from '../../models/invoice';
+
+@Component({
+  selector: 'app-customer-form',
+  standalone: false,
+  templateUrl: './customer-form.component.html',
+  styleUrl: './customer-form.component.scss'
+})
+export class CustomerFormComponent {
+
+  customer: Customer = new Customer();
+  invoice: Invoice = new Invoice();
+}
