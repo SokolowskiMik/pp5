adac3ab082544e4f5ef07cdeff69f416f7dfecd9
s-A301-04 <s-A301-04@CAMPUS.UEK.KRAKOW.PL>
2024-12-02 19:53:23 +0100

    update

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { InvoiceModule } from './invoice/invoice.module';
 import { CommonModule } from '@angular/common';
+import { ProductModuleModule } from './product-module/product-module.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, InvoiceModule],
+  imports: [CommonModule, RouterOutlet, InvoiceModule, ProductModuleModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
